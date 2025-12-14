d217f03ff668224f8e231194410561bead415997
XPRbTcFXMQUYt6Lyh0Rfb8mJ <yomoro2719@discounp.com>
2025-12-14 04:14:47 +0200

    Add public privacy policy page for Meta

---
+++ src/pages/PrivacyPolicy.tsx
@@ -0,0 +1,26 @@
+export default function PrivacyPolicy() {
+  return (
+    <div style={{ maxWidth: 800, margin: '40px auto', padding: 20 }}>
+      <h1>Privacy Policy</h1>
+
+      <p>
+        This application uses Facebook Messenger API to automatically respond
+        to user messages.
+      </p>
+
+      <p>
+        We process messages only to provide automated replies.
+      </p>
+
+      <p>
+        We do not sell or share personal data with third parties.
+      </p>
+
+      <p>
+        Contact:
+        <br />
+        📧 most9276ali@gmail.com
+      </p>
+    </div>
+  );
+}
