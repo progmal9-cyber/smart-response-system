60070cb90fc7a461c499a541d71dbacf2c19a50f
progmal9-cyber <progmal9@gmail.com>
2025-12-26 09:30:31 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -107,7 +107,6 @@ export default function App() {
                       : 'text-gray-600 hover:bg-gray-100'
                   }`}
                 >
-                  
                   <Icon className="w-5 h-5" />
                   <span>{tab.label}</span>
                 </button>
